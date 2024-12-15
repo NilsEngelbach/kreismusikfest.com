d42ec40f4d5e74af721f73940beeb12c49f51262
Nils Engelbach <me@nilsengelbach.com>
2024-12-15 19:31:29 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const targetDate = new Date("2025-05-09T20:00:00").getTime();
+
+    const updateCountdown = () => {
+      const now = new Date().getTime();
+      const difference = targetDate - now;
+
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      setTimeLeft({ days, hours, minutes, seconds });
+    };
+
+    const interval = setInterval(updateCountdown, 1000);
+
+    updateCountdown();
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="flex flex-col items-center justify-center h-full max-w-[800px] mx-auto p-8">
+      <div className="relative h-52 md:h-72 w-full">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/logo.svg"
+          alt="Kreismusikfest 2025"
+          fill={true}
+          style={{ objectFit: "cover" }}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </div>
+      <div className="w-full rounded-2xl flex gap-9 flex-col items-center justify-center bg-cover bg-center">
+        <div className="flex flex-col md:flex-row items-center md:items-start justify-center w-full gap-1.5">
+          <div className="rounded-xl bg-black/25 backdrop-blur-sm py-3 md:min-w-[96px] w-full flex items-center justify-center flex-col gap-1 px-3">
+            <h3 className="font-semibold text-2xl text-white text-center">
+              {timeLeft.days}
+            </h3>
+            <p className="text-lg uppercase font-normal text-white mt-1 text-center w-full">
+              Tage
+            </p>
+          </div>
+          <div className="rounded-xl bg-black/25 backdrop-blur-sm py-3 md:min-w-[96px] w-full flex items-center justify-center flex-col gap-1 px-3">
+            <h3 className="font-semibold text-2xl text-white text-center">
+              {timeLeft.hours}
+            </h3>
+            <p className="text-lg uppercase font-normal text-white mt-1 text-center w-full">
+              Stunden
+            </p>
+          </div>
+          <div className="rounded-xl bg-black/25 backdrop-blur-sm py-3 md:min-w-[96px] w-full flex items-center justify-center flex-col gap-1 px-3">
+            <h3 className="font-semibold text-2xl text-white text-center">
+              {timeLeft.minutes}
+            </h3>
+            <p className="text-lg uppercase font-normal text-white mt-1 text-center w-full">
+              Minuten
+            </p>
+          </div>
+          <div className="rounded-xl bg-black/25 backdrop-blur-sm py-3 md:min-w-[96px] w-full flex items-center justify-center flex-col gap-1 px-3">
+            <h3 className="font-semibold text-2xl text-white text-center">
+              {timeLeft.seconds}
+            </h3>
+            <p className="text-lg uppercase font-normal text-white mt-1 text-center w-full">
+              Sekunden
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
